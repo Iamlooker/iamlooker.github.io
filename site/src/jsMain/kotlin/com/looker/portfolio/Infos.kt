cceb166ca30beb97e9dc5282debf9adc0ea3e496
LooKeR <mohit2002ss@gmail.com>
2024-01-09 12:58:08 +0530

    Update Resume to show PDF

--- site/src/jsMain/kotlin/com/looker/portfolio/Infos.kt
+++ site/src/jsMain/kotlin/com/looker/portfolio/Infos.kt
@@ -4,7 +4,7 @@ const val GitHubProfile = "https://github.com/Iamlooker"
 const val LinkedinProfile = "https://www.linkedin.com/in/iam-looker/"
 const val Mail = "mailto:mohit2002ss@gmail.com"
 const val Telegram = "https://t.me/Iamlooker"
-const val Resume = "https://docs.google.com/document/d/1NjkNQMSettKeFFUXxXAVj-b7lKD6NV-X/edit?usp=sharing&ouid=104970725626530242703&rtpof=true&sd=true"
+const val Resume = "https://drive.google.com/file/d/1rXiM5yfc8dfiFQ79jOcT6MquBaZS2yDj/view?usp=sharing"
 const val UpWork = "https://www.upwork.com/freelancers/~010c4ece8b5853699b"
 
 const val Credits = "Made with Kobweb by LooKeR"
